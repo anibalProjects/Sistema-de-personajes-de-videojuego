0722429a202e6126532068f7a44134f82e4f36be
anibalProjects <anibal.projects@gmail.com>
2025-04-24 10:32:05 +0200

    Estructura de las carpetas

---
+++ VideoJuego/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Requerimientos del problema
+         *Clase Abstracta Personaje
+         */
+        
+        System.out.println("Hello, World!");
+    }
+}
